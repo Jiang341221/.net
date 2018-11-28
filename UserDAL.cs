d53b43aedbbe8133fc9bedb64d3684a24c4da65c
Jiang341221 <45411269+Jiang341221@users.noreply.github.com>
2018-11-28 10:33:09 +0800

    Add files via upload

---
+++ UserDAL.cs
@@ -0,0 +1,13 @@
+﻿using SchoolManage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolManage.DAL
+{
+    public class UserDAL:DAL<User>
+    {
+       
+    }
+}
